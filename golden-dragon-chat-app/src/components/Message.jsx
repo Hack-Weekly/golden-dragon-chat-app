98f5c9e4a9587a48d50d1bf1d5a8a1190dce7727
Trevor <trevor@homesteadcabinet.net>
2023-03-21 07:53:58 -0600

    fix: dev: fixes timestamp formatting issue for new messages
    New messages didn't have a createdAt value, so we now use new Date().toISOString() when createdAt doesn't exist.

--- golden-dragon-chat-app/src/components/Message.jsx
+++ golden-dragon-chat-app/src/components/Message.jsx
@@ -4,6 +4,7 @@ import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 function toDateTime(seconds) {
+  if (!seconds) return new Date().toISOString();
   var t = new Date(1970, 0, 1); // Epoch
   t.setSeconds(seconds);
   return t.toISOString();
@@ -22,7 +23,7 @@ const Message = ({ message }) => {
       <div className="chat-bubble__right">
         <div className="d-flex">
           <p className="user-name pr-1">{message.name}</p>
-          <p className="message-timestamp">{toDateTime(message.createdAt.seconds)}</p>
+          <p className="message-timestamp">{toDateTime(message.createdAt?.seconds)}</p>
         </div>
         <p className="user-message">{message.text}</p>
       </div>
