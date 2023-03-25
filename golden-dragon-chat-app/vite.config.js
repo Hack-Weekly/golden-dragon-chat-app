432a7cb395fd45a7282a3ab2c8daccc0ad742191
Trevor <trevor@homesteadcabinet.net>
2023-03-24 20:58:05 -0600

    Changes to Deploy Properly
    Setup hosting with firebase and deployed to https://golden-dragon-chat-app.web.app/

--- golden-dragon-chat-app/vite.config.js
+++ golden-dragon-chat-app/vite.config.js
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'src',
   build: {
     manifest: true,
+    outDir: 'dist',
     rollupOptions: {
-      input: "./src/main.jsx",
+      input: "index.html",
+      main: "./src/main.jsx",
+      styles: "./src/css/main.css",
     },
   },
 });
