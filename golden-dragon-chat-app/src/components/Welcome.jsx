934b8f948a75670c074b14a0ff13f8a4a46c2f6a
Jackie <44209123+CJaclyn@users.noreply.github.com>
2023-03-26 22:54:25 -0500

    finished welcome component

--- golden-dragon-chat-app/src/components/Welcome.jsx
+++ golden-dragon-chat-app/src/components/Welcome.jsx
@@ -1,15 +1,57 @@
 // The welcome screen shown upon login. Maybe show number of chats since last login or something
-import Sammy from "../img/sammy.jpeg"
-import "../css/main.css"
+import "../css/main.css";
+import React, { useState } from "react";
+import GoogleSignin from "../img/btn_google_signin_dark_normal_web.png";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import FakeMessage from "./FakeMessage";
+import Cat from "../img/cat.jpg";
+import Dog from "../img/dog.png";
+import Capybara from "../img/capybara.png";
 
-export default function SampleComponent() {
+export default function Welcome() {
+  const [user] = useAuthState(auth);
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+
   return (
-  <>
-    <div className="wrapper">
-      <h1>Welcome to the Golden Dragon Chat App</h1>
-      <p>This is an example of a React.js component.</p>
-      <img src={Sammy} alt="Sammy Image" width={200} height={200} />
-    </div>
-  </>
+    <main className="welcome-container">
+      <div className="welcome__left">
+        <h1>Welcome to Golden Dragon Chat</h1>
+        <p>Chat with others around the world!</p>
+        <button className="sign-in">
+          <img
+            onClick={googleSignIn}
+            src={GoogleSignin}
+            alt="sign in with google"
+            type="button"
+          />
+        </button>
+      </div>
+      <div className="welcome__right">
+        <FakeMessage
+          img={Cat}
+          message="hello i'm cat"
+          time="3/20/2023, 15:01"
+          user="Bob123"
+        />
+        <FakeMessage
+          img={Dog}
+          message="howdy ho partner"
+          time="3/20/2023, 15:02"
+          user="Mr_Dog"
+          own={true}
+        />
+        <FakeMessage
+          img={Capybara}
+          message="capybara capybara capybara"
+          time="3/20/2023, 15:02"
+          user="Capy"
+        />
+      </div>
+    </main>
   );
-  }
+}
